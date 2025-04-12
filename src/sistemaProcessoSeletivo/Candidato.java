069cd3485c282101e731a4ac9cfdc22adf6da948
Rafael Santos <rafaelcachorroquentefoda@gmail.com>
2025-04-11 21:31:27 -0300

    coding class Candidato

--- src/sistemaProcessoSeletivo/Candidato.java
+++ src/sistemaProcessoSeletivo/Candidato.java
@@ -1,13 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sistemaProcessoSeletivo;
 
-/**
- *
- * @author ACER
- */
 public class Candidato {
+    private String nome;
+    private double salarioPretendido;
+    private String telefone;
+    private boolean selecionado;
+    private boolean atendeu;
+    
+    public Candidato(){}
+    
+    public Candidato(String n, double s, String tel){
+        this.nome = n;
+        this.salarioPretendido = s;
+        this.telefone = tel;
+    }
+    
+    public void ligar(){
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalarioPretendido() {
+        return salarioPretendido;
+    }
+
+    public void setSalarioPretendido(double salarioPretendido) {
+        if(salarioPretendido < 0){
+            this.salarioPretendido = 0;
+        }else{
+            this.salarioPretendido = salarioPretendido;
+        }
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public boolean isSelecionado() {
+        return selecionado;
+    }
+
+    public void setSelecionado(boolean selecionado) {
+        this.selecionado = selecionado;
+    }
+
+    public boolean isAtendeu() {
+        return atendeu;
+    }
+
+    public void setAtendeu(boolean atendeu) {
+        this.atendeu = atendeu;
+    }
+    
     
 }
